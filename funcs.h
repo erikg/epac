a2dc33f38675667f0d776e5701aeac2a90fa2aa2
erik <erik>
2003-02-18 22:46:46 +0000

    added node and funcs modules

---
+++ funcs.h
@@ -0,0 +1,38 @@
+
+/*****************************************************************************
+ * Erik's Partial Archive Collator                                           *
+ * Copyright (C) 2002-2003 Erik Greenwald <erik@smluc.org>                   *
+ *                                                                           *
+ * This program takes a directory as an argument, then walks through the     *
+ * directory looking for duplicate and partially duplicate files. If it      *
+ * an exact duplicate, it deletes one and creates a hardlink to the other,   *
+ * minimizing disk usage. If it finds a pair of files where they contain the *
+ * same data up to the size of the smaller file, it will prompt if you want  *
+ * to combine them. If you say yes, it will delete the smaller of the files  *
+ * and hardlink to the larger.                                               *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      * 
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this program; if not, write to the Free Software               *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *
+ ****************************************************************************/
+
+/*
+ * $Id: funcs.h,v 1.1 2003/02/18 22:46:46 erik Exp $
+ */
+
+#ifndef __FUNCS_H_
+#define __FUNCS_H_
+
+int eq_i(void *a, void *b);
+
+#endif
